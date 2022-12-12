126f3a894a7526d90a86036c78f2bfb0b126be07
Riley G <gramlichriley@gmail.com>
2022-12-12 13:05:23 -0700

    add django model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model): 
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
